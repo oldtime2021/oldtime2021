ca311ba9eb5a4640a18981f0e9bbeefc8a08ba2d
Hxf <hxf20210701@163.com>
2022-04-26 15:54:01 +0800

    init3

--- src/main/java/com/oldtime/test/Test.java
+++ src/main/java/com/oldtime/test/Test.java
@@ -1,5 +1,6 @@
 package com.oldtime.test;
 
+import com.oldtime.servise.factory.music.IMusicRecommendInterface;
 import com.oldtime.servise.factory.music.RecommendMusicFactory;
 
 public class Test {
@@ -7,6 +8,7 @@ public class Test {
     private static final RecommendMusicFactory factory = new RecommendMusicFactory();
 
     public static void main(String[] args) {
-
+        IMusicRecommendInterface building = factory.building("");
+        building.recommend();
     }
 }
